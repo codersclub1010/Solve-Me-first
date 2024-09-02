bea2d0253fb5ffcc190e25aa1ae489392888f66c
Coder's Club <codersclub1010@gmail.com>
2024-09-02 20:04:19 +0530

    Add files via upload

---
+++ solvMeFirst.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    // complete the solveMeFirst function and a return a number
+}
+
+int main() {
+    int num1,num2;
+    cin>>num1>>num2; 
+    int sum = solveMeFirst(num1,num2);
+    cout<<(sum);
+    return 0;
+}
---
+++ solveMeFirst.py
@@ -0,0 +1,8 @@
+def solveMeFirst(a,b):
+    pass
+	# complete the function and return a number
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
