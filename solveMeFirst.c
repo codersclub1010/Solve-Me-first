f98d30e323951611a9f201a624b32c071d0e3b22
Coder's Club <codersclub1010@gmail.com>
2024-09-02 20:04:55 +0530

    Add files via upload

---
+++ solveMeFirst.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int solveMeFirst(int a, int b) {
+    // complete the solveMeFirst function and a return a number
+}
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    int sum; 
+    sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
