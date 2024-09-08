1135b349b0d5e568b454b74aa6d906ecd6ef31b0
Coder's Club <codersclub1010@gmail.com>
2024-09-08 17:45:42 +0530

    sol

--- solveMeFirst.c
+++ solveMeFirst.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 
 int solveMeFirst(int a, int b) {
-    // complete the solveMeFirst function and a return a number
+    int sum;
+    sum = a + b;
+    return sum;
 }
 
 int main() {
-    int num1,num2;
-    scanf("%d %d",&num1,&num2);
+    int num1,num2;//Constraints for taking user input
+    printf("Enter first number (1 to 1000): ");
+    scanf("%d",&num1);
+    if(num1>1000){
+        printf("Invalid input");
+        printf("Enter the first number Again : ");
+        scanf("%d",&num1);
+    }
+    printf("Enter second number (1 to 1000): ");
+    scanf("%d",&num2);
+    if(num2>1000){
+        printf("Invalid input");
+        printf("Enter the second number Again : ");
+        scanf("%d",&num2);
+    }
     int sum; 
     sum = solveMeFirst(num1,num2);
-    printf("%d",sum);
+    printf("Sum of two number is : %d",sum);
     return 0;
 }
