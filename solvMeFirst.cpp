720e92e622deced3502404e4c5b3709743cd7c47
Coder's Club <codersclub1010@gmail.com>
2024-09-08 17:44:12 +0530

    Solution

--- solvMeFirst.cpp
+++ solvMeFirst.cpp
@@ -1,14 +1,26 @@
-#include <iostream.h>
+#include <iostream>
 using namespace std;
 
 int solveMeFirst(int a, int b) {
-    // complete the solveMeFirst function and a return a number
+    int sum=a+b;
+    return sum;
 }
 
 int main() {
-    int num1,num2;
-    cin>>num1>>num2; 
+    int num1,num2;//Constraints for taking user input
+    cout<<"Enter the first number (1 to 1000): ";
+    cin>>num1;
+    if(num1>1000) {
+        cout<<"Enter the first number Again : ";
+        cin>>num1;
+    }
+    cout<<"Enter the second number (1 to 1000): ";
+    cin>>num2;
+    if(num2>1000){
+        cout<<"Enter the second number Again : ";
+        cin>>num2;
+    }
     int sum = solveMeFirst(num1,num2);
-    cout<<(sum);
+    cout<<("Sum of two number is : ",sum);
     return 0;
 }
